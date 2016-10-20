13db9444601c98ca33ca3ba8eb7986b59166023e
Aamond <alex.prost@dlmsoft.fr>
2016-10-20 23:32:20 +0200

    Version 20/10/2016 23h

---
+++ MTG_DeckTester/UserControls/uc_Deck.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MTG_DeckTester.UserClasses;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MTG_DeckTester.UserControls
+{
+    /// <summary>
+    /// Interaction logic for uc_Board_Deck.xaml
+    /// </summary>
+    public partial class uc_Board_Deck : UserControl
+    {
+        public Deck Deck_List;
+        public Graveyard Graveyard_List;
+        public Exile Exile_List;
+
+        public uc_Board_Deck()
+        {
+            InitializeComponent();
+
+            img_deck_card.Source = new BitmapImage(new Uri(Tools.GetPath(ConfigKeys.CARDS) + "000000.jpg", UriKind.RelativeOrAbsolute));
+            img_graveyard_card.Source = new BitmapImage(new Uri(Tools.GetPath(ConfigKeys.CARDS) + "000000.jpg", UriKind.RelativeOrAbsolute));
+            img_exile_card.Source = new BitmapImage(new Uri(Tools.GetPath(ConfigKeys.CARDS) + "000000.jpg", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
